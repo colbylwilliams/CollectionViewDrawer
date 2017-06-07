f09c7e91a3a21a26ffefdf798e49a57e5fb69640
Colby Williams <colbylwilliams@gmail.com>
2017-06-06 21:27:16 -0400

    set up custom components for collection view drawer on iOS

---
+++ CollectionViewDrawer/CollectionViewDrawer.iOS/UICollectionViewDrawerFlowLayout.cs
@@ -0,0 +1,16 @@
+﻿﻿using System;
+using Foundation;
+using UIKit;
+
+namespace CollectionViewDrawer.iOS
+{
+	[Register ("UICollectionViewDrawerFlowLayout")]
+	public class UICollectionViewDrawerFlowLayout : UICollectionViewFlowLayout
+	{
+		public UICollectionViewDrawerFlowLayout (IntPtr handle) : base (handle)
+		{
+		}
+
+
+	}
+}
